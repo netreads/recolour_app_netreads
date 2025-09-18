a3a6bc5a3bd8341942a89ba799f168e93815144b
Yash Patel <yash@Yashs-MacBook-Air.local>
2025-09-18 22:05:12 +0530

    removed unused files

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -1,4 +1,2 @@
 export { ImageCard } from './ImageCard';
 export { ImageViewer } from './ImageViewer';
-export { ImageComparison } from './ImageComparison';
-export { UploadForm } from './UploadForm';
