3d17b9cc2f6cf5a860677079ec7d06c2edecb772
Netreads <azcontent101@gmail.com>
2025-09-21 00:23:58 +0530

    reopened HEAD and OPTION

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -8,5 +8,5 @@ export const GET = auth.handler;
 export const POST = auth.handler;
 
 // Explicitly export head/options for Next.js routing and CORS preflight
-// export const HEAD = auth.handler as unknown as typeof GET;
-// export const OPTIONS = auth.handler as unknown as typeof GET;
+export const HEAD = auth.handler as unknown as typeof GET;
+export const OPTIONS = auth.handler as unknown as typeof GET;
